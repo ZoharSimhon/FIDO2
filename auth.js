4e78298dd774d2b8b468d51f41b24f2c48119fe0
ZoharSimhon <Zoharsimhon13@gmail.com>
2024-08-06 15:52:17 +0300

    check username exists

--- auth.js
+++ auth.js
@@ -1,11 +1,23 @@
+const users = {
+    lecturer: { password: 'lecturer', role: 'lecturer' },
+    student1: { password: 'student1', role: 'student' },
+    student2: { password: 'student2', role: 'student' },
+    student3: { password: 'student3', role: 'student' }
+};
+
 function checkLogin() {
     const role = localStorage.getItem('role');
+    const username = localStorage.getItem('username');
     const path = window.location.pathname;
 
     if (role === 'student' && path.includes('view-attendance.html')) {
         window.location.href = 'mark-attendance.html';
     } else if (role === 'lecturer' && path.includes('mark-attendance.html')) {
         window.location.href = 'view-attendance.html';
+    }
+
+    if (!username in users ){
+        window.location.href = 'index.html';
     }
 }
 
